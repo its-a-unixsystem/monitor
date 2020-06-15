82ce415babb43034d2b241816f7f30d6e417524e
Thomas Uebermeier <66879848+thomas-for-aiven@users.noreply.github.com>
2020-06-15 13:43:11 +0200

    initial config files + update DB

--- deploy/checks.sql
+++ deploy/checks.sql
@@ -7,6 +7,7 @@ CREATE TABLE monitor.checks (
     name      varchar(255) NOT NULL,
     host      varchar(255) NOT NULL,
     checktime timestamp NOT NULL,
+    response_time integer,
     regex_hits integer,
     regex varchar(255),
     return_code smallint
