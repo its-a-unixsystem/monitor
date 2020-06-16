436347e7ed8ca671746af563c7df18283749ef44
Thomas <thomas-for-aiven@februus.net>
2020-06-16 13:26:09 +0200

    fixup! udpated docs, tests, structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# from https://packaging.python.org/tutorials/packaging-projects/
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="monitor-pkg-thomas-uebermeier",
+    version="0.9.1",
+    author="Thomas Uebermeier",
+    author_email="thomas@februus.net",
+    description="Website monitor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thomas-for-aiven/monitor",
+    packages=setuptools.find_packages(),
+    scripts=['monitor/consumtodb.py', 'monitor/checker.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
